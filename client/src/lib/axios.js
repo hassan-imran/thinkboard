10520249faf5901dd0a065e3485c7523ef4bd447
Hassan Imran <43490649+hassan-imran@users.noreply.github.com>
2025-07-06 20:54:25 +0500

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    // baseURL: "http://localhost:5001/api", // For development
+    baseURL: "https://thinkboard-api.vercel.app/api"
 });
 
 export default api;
