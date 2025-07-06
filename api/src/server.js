79481b6c47fe792391a7ff23bfa3d0ea9bb8f106
Hassan Imran <43490649+hassan-imran@users.noreply.github.com>
2025-07-06 19:24:18 +0500

    For vercel deployment

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,13 +12,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 
 // app.get("/api/notes", (req, res)=> {
 //     res.status(200).send("Lorem ipsum")
 // })
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors());
+}
+
 app.use(express.json()); // Middleware to get the req parameters
 
 // Custom middleware
@@ -28,6 +32,14 @@ app.use(express.json()); // Middleware to get the req parameters
 
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { Route, Routes } from "react-router";
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import NoteDetailPage from './pages/NoteDetailPage';
-import toast from 'react-hot-toast';
 
 const App = () => {
   return (
@@ -13,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
-        <Route path='/note/:id' element={<NoteDetailPage />} />
+        <Route path='/notes/:id' element={<NoteDetailPage />} />
         
       </Routes>
     </div>
